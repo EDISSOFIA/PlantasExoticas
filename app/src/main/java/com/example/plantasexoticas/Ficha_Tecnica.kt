60ce5e6e43bcaa5571de64c67f2e9c82d96139b3
EDISSOFIA <edso95@gmail.com>
2022-10-21 17:43:29 -0500

    version actualizada

---
+++ app/src/main/java/com/example/plantasexoticas/Ficha_Tecnica.kt
@@ -0,0 +1,11 @@
+package com.example.plantasexoticas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ficha_Tecnica : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ficha_tecnica)
+    }
+}
--- app/src/main/java/com/example/plantasexoticas/MainActivity.kt
+++ app/src/main/java/com/example/plantasexoticas/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.plantasexoticas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var nameUserEdit: EditText? = null
+    private var passUserEdit: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nameUserEdit = findViewById(R.id.nameUserEdit)
+        passUserEdit = findViewById(R.id.passUserEdit)
     }
+    fun loginUser(botonUserEdit:View) {
+        if (nameUserEdit!!.text.toString() == "edis@gmail.com"){
+            if (passUserEdit!!.text.toString() == "12345") {
+                val intento = Intent(this, Noticias::class.java)
+                startActivity(intento)
+            }
+        }
+    }
+
 }
