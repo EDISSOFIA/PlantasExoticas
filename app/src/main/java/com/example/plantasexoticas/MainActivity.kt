511509cfcdf7941ec2c29db1e13a05d70af860ed
Erika Lozano <112278946+erikard6@users.noreply.github.com>
2022-11-09 20:49:09 -0500

    Implementando Navigation Drawer

--- app/src/main/java/com/example/plantasexoticas/MainActivity.kt
+++ app/src/main/java/com/example/plantasexoticas/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.content.Intent
 import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import android.content.DialogInterface
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var nameUserEdit: EditText? = null
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         nameUserEdit = findViewById(R.id.nameUserEdit)
         passUserEdit = findViewById(R.id.passUserEdit)
     }
@@ -20,7 +24,12 @@ class MainActivity : AppCompatActivity() {
             if (passUserEdit!!.text.toString() == "12345") {
                 val intento = Intent(this, Noticias::class.java)
                 startActivity(intento)
+                Toast.makeText(applicationContext,getString(R.string.txt_welcome),Toast.LENGTH_LONG).show()
+
             }
+        }else{
+            Toast.makeText(applicationContext,getResources().getString(R.string.txt_men2),Toast.LENGTH_SHORT).show()
+
         }
     }
 
--- app/src/main/java/com/example/plantasexoticas/Noticias.kt
+++ app/src/main/java/com/example/plantasexoticas/Noticias.kt
@@ -2,10 +2,72 @@ package com.example.plantasexoticas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
 
 class Noticias : AppCompatActivity() {
+    private lateinit var appbarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_noticias)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+       /* if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view,ToDoFragment::class.java,null,"todo")
+                .commit()
+        }*/
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        appbarConfiguration = AppBarConfiguration(setOf(R.id.nav_todo,R.id.nav_about),drawerLayout)
+        setupActionBarWithNavController(navController, appbarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp(appbarConfiguration) || super.onSupportNavigateUp()
+    }
+/*
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menuprincipal,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        R.id.action_search ->{
+            Toast.makeText(this,R.string.txt_action_search, Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_settings ->{
+            Toast.makeText(this,R.string.txt_action_settings, Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_logout ->{
+            Toast.makeText(this,R.string.txt_action_logout, Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.action_email ->{
+            Toast.makeText(this,R.string.txt_action_email, Toast.LENGTH_LONG).show()
+            true
+        }
+        else ->{
+            super.onOptionsItemSelected(item)
+        }
+    }*/
+
 }
